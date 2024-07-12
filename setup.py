81df5c1c230581f0c72964727969e8a7e3bfbe05
AMarsel2551 <m.adbullinn@gmail.com>
2024-07-12 11:28:52 +0300

    edit directory

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ def get_version(package):
     """
     Return package version as listed in `__version__` in `init.py`.
     """
-
     with open(os.path.join(package, "__init__.py")) as f:
         return re.search("__version__ = ['\"]([^'\"]+)['\"]", f.read()).group(1)
 
