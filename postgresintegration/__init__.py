672e8c4b6859c455a452b9d41584e60dc5694a43
AMarsel2551 <m.adbullinn@gmail.com>
2024-07-12 11:47:04 +0300

    edit directory

--- postgresintegration/__init__.py
+++ postgresintegration/__init__.py
@@ -5,6 +5,6 @@ __status__ = "production"
 __version__ = "0.0.1"
 __date__ = "12 July 2024"
 
-from .main import DatabaseConnector, PostgresFunctionGenerator
+from .main import PostgresFunctionGenerator
 
-__all__ = ["DatabaseConnector", "PostgresFunctionGenerator"]
+__all__ = ["PostgresFunctionGenerator"]
